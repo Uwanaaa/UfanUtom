fbaaaad2c5d14d9c9b441a2395cfaf8b968c5826
Uwanaaa <uwanaudofia8@gmail.com>
2025-10-24 12:11:44 +0100

    Modified the settings to increase interopertability

--- ufanutom/urls.py
+++ ufanutom/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/', include('main.urls')),
     path('mail/',include('mail.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
